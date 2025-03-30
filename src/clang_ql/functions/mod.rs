8013c3cd91f4557ffc14f2be874eecdd838f961b
AmrDeveloper <amr96@programmer.net>
2025-03-30 15:52:40 +0200

    Remove functions std to depend only on the pattern matchers API

--- src/clang_ql/functions/mod.rs
+++ src/clang_ql/functions/mod.rs
@@ -6,7 +6,6 @@ use gitql_core::signature::StandardFunction;
 use gitql_std::standard::standard_function_signatures;
 use gitql_std::standard::standard_functions;
 
-mod ast;
 mod matchers;
 
 #[inline(always)]
@@ -14,7 +13,6 @@ pub fn clang_ql_functions() -> &'static HashMap<&'static str, StandardFunction> 
     static HASHMAP: OnceLock<HashMap<&'static str, StandardFunction>> = OnceLock::new();
     HASHMAP.get_or_init(|| {
         let mut map = standard_functions().to_owned();
-        ast::register_ast_functions(&mut map);
         matchers::register_matchers_functions(&mut map);
         map
     })
@@ -23,7 +21,6 @@ pub fn clang_ql_functions() -> &'static HashMap<&'static str, StandardFunction> 
 #[inline(always)]
 pub fn clang_ql_functions_signatures() -> HashMap<&'static str, Signature> {
     let mut map = standard_function_signatures().to_owned();
-    ast::register_ast_signatures(&mut map);
     matchers::register_matchers_signatures(&mut map);
     map
 }
